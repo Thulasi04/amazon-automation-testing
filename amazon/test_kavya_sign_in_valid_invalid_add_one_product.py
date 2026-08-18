564351977dc002a498f56c715b598446c138fd78
IU23IT050-Kavya <kvz.xx777@gmail.com>
2026-08-18 17:57:40 +0530

    signin invalid and valid

--- amazon/test_kavya_sign_in_valid_invalid_add_one_product.py
+++ amazon/test_kavya_sign_in_valid_invalid_add_one_product.py
@@ -2,3 +2,16 @@ from playwright.sync_api import Page
 def test_open_amazon(page: Page):
     page.goto("https://www.amazon.com/")
     page.locator("span[class='nav-line-2 ']").click()
+    page.locator("[type='email']").type("invalidusername")
+    page.locator('[type="submit"]').click()
+    page.locator('[class="a-icon a-icon-close"]').click()
+    page.locator('[type="email"]').click()
+    page.locator('[type="email"]').type("kvz.xx777@gmail.com")
+    page.locator('[type="submit"]').click()
+    page.locator('[type="password"]').type("Kavya@automation")
+    page.locator('[type="submit"]').click()
+    page.locator('[type="text"]').type("iphone")
+    page.locator('[id="nav-search-submit-button"]').click()
+    page.locator('[name="submit.addToCart"]').click()
+    page.locator('[type="submit"]').click()
+
