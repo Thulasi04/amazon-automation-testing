5645dce979f28bb739e538acf68a165251edc1b1
Thulasi <thulasiudhaya41204@gmail.com>
2026-08-18 18:18:55 +0530

    to add five baby products

--- amazon/test_thulasi_add_five_baby_produts_and_goto_cart.py
+++ amazon/test_thulasi_add_five_baby_produts_and_goto_cart.py
@@ -1,45 +1,21 @@
 from playwright.sync_api import Page
 
+def test_thulasi(page: Page):
 
-def test_thulasi_add_five_baby_products_and_goto_cart(page: Page):
-
-    # Open Amazon
+#login
     page.goto("https://www.amazon.in/")
+    #page.locator("[id='nav-link-accountList']").click()
+    #page.locator("[type = 'email']").type("thulasiudhaya41204@gmail.com")
+    #page.locator("[class='a-button-input']").click()
 
-    # Login
-    page.locator("#nav-link-accountList").click()
-    page.locator("input[type='email']").fill("thulasiudhaya41204@gmail.com")
-    page.locator("#continue").click()
-    page.locator("input[type='password']").fill("mugunthan")
-    page.locator("#signInSubmit").click()
+    #page.locator("[type = 'password']").type("mugunthan")
+    #page.locator("[class='a-button-input']").click()
 
-    # Search baby products
-    page.locator("#twotabsearchtextbox").fill("baby products")
-    page.locator("#nav-search-submit-button").click()
+#all
+    page.locator("i[class='hm-icon nav-sprite']").click()
+    page.locator("[data-ref-tag='nav_em_1_1_1_12']").click()
+    page.locator("[href='/gp/browse.html?node=1571274031&ref_=nav_em_sbc_tbk_baby_products_0_2_15_3']").click()
 
-    # Product 1
-    page.locator("[data-component-type='s-search-result'] h2 a").nth(0).click()
-    page.locator("#add-to-cart-button").click()
-    page.goto("https://www.amazon.in/s?k=baby+products")
-
-    # Product 2
-    page.locator("[data-component-type='s-search-result'] h2 a").nth(1).click()
-    page.locator("#add-to-cart-button").click()
-    page.goto("https://www.amazon.in/s?k=baby+products")
-
-    # Product 3
-    page.locator("[data-component-type='s-search-result'] h2 a").nth(2).click()
-    page.locator("#add-to-cart-button").click()
-    page.goto("https://www.amazon.in/s?k=baby+products")
-
-    # Product 4
-    page.locator("[data-component-type='s-search-result'] h2 a").nth(3).click()
-    page.locator("#add-to-cart-button").click()
-    page.goto("https://www.amazon.in/s?k=baby+products")
-
-    # Product 5
-    page.locator("[data-component-type='s-search-result'] h2 a").nth(4).click()
-    page.locator("#add-to-cart-button").click()
-
-    # Go to cart
-    page.locator("#nav-cart").click()
+#1stproduct
+    page.locator("[data-testid='add-to-cart-button']").click()
+    page.locator("[data-testid='add-to-cart-variational-modal-button']").click()
